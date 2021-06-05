8a73bd65c376c60691d8b3bdf73a82f688b586c9
Sayantan Basu <sayantanbasu@live.in>
2021-06-05 14:50:40 -0400

    updated index2.js

--- index2.js
+++ index2.js
@@ -1,8 +1,17 @@
-const {fetchMyIP} = require('./iss_promised');
-const {fetchCoordsByIP} = require('./iss_promised');
+const {nextISSTimesForMyLocation} = require('./iss_promised');
 
-fetchMyIP()
-.then (fetchCoordsByIP)
-.then(body => console.log(body));
+
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+// see index.js for printPassTimes 
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call 
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  }).catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
 
 
