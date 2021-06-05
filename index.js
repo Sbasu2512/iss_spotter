b96a06bd804fca5a8ef186b8a54118e62f26cd85
Sayantan Basu <sayantanbasu@live.in>
2021-06-05 08:03:28 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,36 +1,45 @@
-const { fetchISSFlyOverTimes } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
-const { fetchMyIP } = require('./iss');
+// const { fetchISSFlyOverTimes } = require('./iss');
+// const { fetchCoordsByIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
 
 
-fetchMyIP((error, ip) => {                //(error, ip) => anonymous calllback fn!
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-  console.log(ip);
-});
+// fetchMyIP((error, ip) => {                //(error, ip) => anonymous calllback fn!
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log(ip);
+// });
 
 
 
-fetchCoordsByIP('142.114.24.119' , (error, data) => {
+// fetchCoordsByIP('142.114.24.119' , (error, data) => {
   
-if(error) {
-  console.log("It didn't work!", error);
-  return;
-}
-console.log("it Worked! Geolocation is:", data);
-});
+// if(error) {
+//   console.log("It didn't work!", error);
+//   return;
+// }
+// console.log("it Worked! Geolocation is:", data);
+// });
 
-let location = { lon: -79.3644, latti: 43.7124 };
+// let location = { lon: -79.3644, latti: 43.7124 };
 
 
-//this is just calling the function from iss.js
-fetchISSFlyOverTimes(location,(error,data)=> {
+// //this is just calling the function from iss.js
+// fetchISSFlyOverTimes(location,(error,data)=> {
+//   if(error){
+//     console.log("It didnt work", error);
+//     return;
+//   }
+//   console.log("it worked! flyOverTimes is:", data);
+// });
+
+nextISSTimesForMyLocation((error,data)=>{
   if(error){
-    console.log("It didnt work", error);
+    console.log(error);
     return;
   }
-  console.log("it worked! flyOverTimes is:", data);
-});
+  console.log("it worked!", data);
+})
