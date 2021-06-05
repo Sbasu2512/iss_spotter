47a96fb611c91cfb78ca32cad9f384f9fc501a3c
Sayantan Basu <sayantanbasu@live.in>
2021-06-05 06:22:13 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+const { fetchCoordsByIP } = require('./iss');
 const { fetchMyIP } = require('./iss');
 
 fetchMyIP((error, ip) => {                //(error, ip) => anonymous calllback fn!
@@ -10,4 +10,12 @@ fetchMyIP((error, ip) => {                //(error, ip) => anonymous calllback f
 
   console.log('It worked! Returned IP:' , ip);
 });
-//fetchMyIP();
+
+fetchCoordsByIP(ip, (data,error) => {
+if(error) {
+  console.log("It didn't work!", error);
+  return;
+}
+console.log("it Worked! Geolocation is:", data);
+});
+
