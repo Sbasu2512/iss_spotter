d94d29a25935ad8e2089b54bd8aa03016e0c1fbe
Sayantan Basu <sayantanbasu@live.in>
2021-06-04 22:45:25 -0400

    index.js: should show body

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const request = require("request");
+
 const fetchMyIP = function(callback) { 
   // use request to fetch IP address from JSON API
-  
+  request("https://api.ipify.org/?format=json", (error,response,body)=>{
+  console.log(body);
+  });
 }
 
 module.exports = { fetchMyIP };
