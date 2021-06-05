7e4f77e59774ebcfeae57433c3b0c97562ea97de
Sayantan Basu <sayantanbasu@live.in>
2021-06-05 14:32:59 -0400

    added fetchmycoords to both of them

--- iss_promised.js
+++ iss_promised.js
@@ -3,4 +3,9 @@ const fetchMyIP = () => {
   return request('https://api.ipify.org?format=json'); 
   }
 
-  module.exports = { fetchMyIP} ;
+const fetchCoordsByIP = (body) =>{
+const ip = JSON.parse(body).ip;
+return request(`https://freegeoip.app/json/${ip}`);
+}
+  
+  module.exports = { fetchMyIP, fetchCoordsByIP} ;
