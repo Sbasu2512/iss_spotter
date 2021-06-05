5686792b4aaf474dad835fe0227a02c3c5ac6d93
Sayantan Basu <sayantanbasu@live.in>
2021-06-05 06:52:59 -0400

    updated index.js

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 const { fetchCoordsByIP } = require('./iss');
 const { fetchMyIP } = require('./iss');
 
+
+
 fetchMyIP((error, ip) => {                //(error, ip) => anonymous calllback fn!
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-
-  console.log('It worked! Returned IP:' , ip);
+  console.log(ip);
 });
 
-fetchCoordsByIP(ip, (data,error) => {
+
+
+fetchCoordsByIP('142.114.24.119' , (error, data) => {
+  
 if(error) {
   console.log("It didn't work!", error);
   return;
